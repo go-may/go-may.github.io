f309ceb0910ca5b1c4acc7418f3f6471b4c19069
go-may <75523685+go-may@users.noreply.github.com>
2020-12-05 14:14:25 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$to = "davittchemi555@gmail.com";
-	$subject = "Приветствие на сайте";
-	$message = "Приветствуем вас на сайте index.php";
-	mail($to, $subject, $message);
-	echo "Ваше сообщение было отправлено";
- ?>
